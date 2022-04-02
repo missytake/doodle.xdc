25942947d39f76bc86007c0d6bc05c7129218841
missytake <missytake@systemli.org>
2022-04-02 19:11:42 +0200

    specify types

--- types.d.ts
+++ types.d.ts
@@ -3,13 +3,21 @@
 export type StatusUpdate =
   | {
       method: "setTimerange";
+      day: number; // 0-6
+      start: number;  // 0-95
+      end: number;  // 0-95
+      uid: number;  // some unique ID
+      owner: string; // window.webxdc.selfAddr
     }
   | {
       method: "setTitle";
+      title: string;
     }
   | {
       method: "setDeadline";
+      deadline: Date;
     }
   | {
       method: "deleteTimerange";
+      uid: number;  // the uid of the timerange you want to delete
     };
