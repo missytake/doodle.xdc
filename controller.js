d301e9f4a6c535f764473056b693a11f3dc9fe75
missytake <missytake@systemli.org>
2022-04-03 10:12:52 +0200

    fix javascript fuckup

--- controller.js
+++ controller.js
@@ -1,12 +1,12 @@
 function setTimerange(start, end) {
-    payload = {
+    const payload = {
         method: "setTimerange",
         start: start,
         end: end,
         owner: window.webxdc.selfAddr,
         ownername: window.webxdc.selfName,
-        uid: owner + performance.now,
+        uid: window.webxdc.selfAddr + performance.now,
     }
-    descr = payload.owner + " chose timeslot " + start + " until " + end
+    const descr = payload.owner + " chose timeslot " + start + " until " + end
     window.webxdc.sendUpdate(payload, descr)
 }
